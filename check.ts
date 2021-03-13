d5847e7d152da8444ea918f963a6fb89e2e22ad5
duart38 <mr.codercuracao@gmail.com>
2021-03-13 10:48:16 +0100

    updated imports

--- check.ts
+++ check.ts
@@ -1,34 +1,6 @@
 import { config } from "./config.js";
 import { Notify } from "./notify.ts";
-
-interface command {
-  name: string;
-  regex: RegExp;
-}
-const COMMANDS: command[] = [
-  { name: "cd", regex: /cd\s/g },
-  { name: "cp", regex: /cp\s/g },
-  { name: "rm", regex: /rm\s/g },
-  { name: "ls", regex: /ls\s/g },
-  { name: "cat", regex: /cat\s/g },
-  { name: "less", regex: /less\s/g },
-  { name: "echo", regex: /echo\s/g },
-  { name: "date", regex: /date/g },
-  { name: "sh", regex: /sh\s/g },
-  { name: "mkdir", regex: /mkdir\s/g },
-  { name: "touch", regex: /touch\s/g },
-  { name: "|", regex: /\|/g },
-  { name: ">", regex: />/g },
-  { name: ">>", regex: />>/g },
-  { name: "$", regex: /\$/g },
-  { name: "&", regex: /&/g },
-  { name: "ssh", regex: /ssh\s/g },
-  { name: "sudo", regex: /sudo\s/g },
-  { name: "chmod", regex: /chmod\s/g },
-  { name: "chown", regex: /chown\s/g },
-  { name: "clear", regex: /clear\s/g },
-  { name: "curl", regex: /curl\s/g },
-];
+import {COMMANDS} from "./data/commands.ts";
 
 const RANGES = levelsToRange(levels(config.levels)).reverse();
 export function checkLevel(data: Record<string, number>) {
