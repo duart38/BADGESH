0be08f01253042fbec524340ec1236b65d9e3927
duart38 <mr.codercuracao@gmail.com>
2021-03-14 14:23:00 +0100

    added cli

--- CLI.ts
+++ CLI.ts
@@ -1,58 +1,77 @@
 import Interpolation from "https://raw.githubusercontent.com/duart38/Interpolate/master/Interpolate.ts";
+import { loadOrCreate } from "./utils/utils.ts";
+import { config } from "./config.js";
 export default class CLI {
-    private screen: Interpolation;
-    private achievements: string[] = [];
-    constructor(){
-        this.screen = new Interpolation(`
-         ___           ___           ___           ___           ___           ___           ___     
-        /\  \         /\  \         /\  \         /\  \         /\  \         /\  \         /\__\    
-       /::\  \       /::\  \       /::\  \       /::\  \       /::\  \       /::\  \       /:/  /    
-      /:/\:\  \     /:/\:\  \     /:/\:\  \     /:/\:\  \     /:/\:\  \     /:/\ \  \     /:/__/     
-     /::\~\:\__\   /::\~\:\  \   /:/  \:\__\   /:/  \:\  \   /::\~\:\  \   _\:\~\ \  \   /::\  \ ___ 
-    /:/\:\ \:|__| /:/\:\ \:\__\ /:/__/ \:|__| /:/__/_\:\__\ /:/\:\ \:\__\ /\ \:\ \ \__\ /:/\:\  /\__\
-    \:\~\:\/:/  / \/__\:\/:/  / \:\  \ /:/  / \:\  /\ \/__/ \:\~\:\ \/__/ \:\ \:\ \/__/ \/__\:\/:/  /
-     \:\ \::/  /       \::/  /   \:\  /:/  /   \:\ \:\__\    \:\ \:\__\    \:\ \:\__\        \::/  / 
-      \:\/:/  /        /:/  /     \:\/:/  /     \:\/:/  /     \:\ \/__/     \:\/:/  /        /:/  /  
-       \::/__/        /:/  /       \::/__/       \::/  /       \:\__\        \::/  /        /:/  /   
-        ~~            \/__/         ~~            \/__/         \/__/         \/__/         \/__/    
-    -------------------------------------------------------------------------------------------------
-        {{Achievements1}}                                                 {{Achievements2}}          
-    -------------------------------------------------------------------------------------------------
-        {{Stats}}                                                                                    
-                                                                                                     
-        `);
+  private screen: Interpolation;
+  private achievements: string[];
+  constructor(loadFromFile: boolean) {
+    this.screen = new Interpolation(`\n\n
+\t     ___           ___           ___           ___           ___           ___           ___     
+\t    /\\  \\         /\\  \\         /\\  \\         /\\  \\         /\\  \\         /\\  \\         /\\__\\    
+\t   /::\\  \\       /::\\  \\       /::\\  \\       /::\\  \\       /::\\  \\       /::\\  \\       /:/  /    
+\t  /:/\\:\\  \\     /:/\\:\\  \\     /:/\\:\\  \\     /:/\\:\\  \\     /:/\\:\\  \\     /:/\\ \\  \\     /:/__/     
+\t /::\\~\\:\\__\\   /::\\~\\:\\  \\   /:/  \\:\\__\\   /:/  \\:\\  \\   /::\\~\\:\\  \\   _\\:\\~\\ \\  \\   /::\\  \\ ___ 
+\t/:/\\:\\ \\:|__| /:/\\:\\ \\:\\__\\ /:/__/ \\:|__| /:/__/_\\:\\__\\ /:/\\:\\ \\:\\__\\ /\\ \\:\\ \\ \\__\\ /:/\\:\\  /\\__\\
+\t\\:\\~\\:\\/:/  / \\/__\\:\\/:/  / \\:\\  \\ /:/  / \\:\\  /\\ \\/__/ \\:\\~\\:\\ \\/__/ \\:\\ \\:\\ \\/__/ \\/__\\:\\/:/  /
+\t \\:\\ \\::/  /       \\::/  /   \\:\\  /:/  /   \\:\\ \\:\\__\\    \\:\\ \\:\\__\\    \\:\\ \\:\\__\\        \\::/  / 
+\t  \\:\\/:/  /        /:/  /     \\:\\/:/  /     \\:\\/:/  /     \\:\\ \\/__/     \\:\\/:/  /        /:/  /  
+\t   \\::/__/        /:/  /       \\::/__/       \\::/  /       \\:\\__\\        \\::/  /        /:/  /   
+\t    ~~            \\/__/         ~~            \\/__/         \\/__/         \\/__/         \\/__/    
+\t------------------------------------------------------------------------------------------------------
+{{Achievements}}                                                                                        
+\t------------------------------------------------------------------------------------------------------
+`);
+    this.achievements = [];
+    this.flush();
+    if (loadFromFile) this.loadFromFile();
+  }
 
-        this.flush();
+  /**
+   * Adds achievement. does not print
+   * @param txt
+   */
+  public addAchievement(txt: string) {
+    this.achievements.push(txt);
+  }
+
+  public loadFromFile() {
+    const ACHIEVEMENTS: {
+      [x: string]: { givenRanges: number[]; textValues: string[] };
+    } = JSON.parse(loadOrCreate(config.db.achievements, "{}"));
+    Object.values(ACHIEVEMENTS).forEach((val) =>
+      val.textValues.forEach((x) => this.addAchievement(x))
+    );
+
+    this.print();
+  }
+
+  public print() {
+    let a = ``;
+    const sortedAchievements = [...this.achievements].sort((a, b) => b.length - a.length);
+    const longestLength = sortedAchievements[0].trimEnd().length || 0;
+
+    function paddedFill(data: string, charAmount: number) {
+      if (data.length < charAmount) return data + " ".repeat(charAmount - data.length);
+      else return data.substring(0,charAmount - 3) + "...";
     }
 
-    /**
-     * Adds achievement to screen boi
-     * @param txt 
-     */
-    public addAchievement(txt: string){
-        this.achievements.push(txt);
-        const t = ``;
-
-        this.achievements.forEach((str)=>{
-            
-        })
-
+    for (let i = 0; i < sortedAchievements.length - 1; i += 2) {
+      const left = this.achievements[i] || "";
+      const right = this.achievements[i + 1];
+      a += "\t| " + paddedFill(left, longestLength - 1) + " | " + paddedFill(right, 97 - longestLength) + "|\n";
     }
+    this.screen.reset();
+    this.screen.fill("Achievements", a);
+  }
 
-    public loadFromFile(file: string){
-
-    }
-
-    /**
-     * Flushes the toilet to stdout (the toilet hole).
-     * anyways.. only to be called once... you know the drill...
-     */
-    private flush(){
-        this.screen.bind((x)=>{
-            console.clear();
-            console.log(x);
-        })
-    }
-    
-
+  /**
+   * Flushes the toilet to stdout (the toilet hole).
+   * anyways.. only to be called once... you know the drill...
+   */
+  private flush() {
+    this.screen.bind((x) => {
+      console.clear();
+      console.log(x);
+    });
+  }
 }
